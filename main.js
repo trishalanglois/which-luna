4ae0bdd5fcb02b4cb59246eac5caf90565327838
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-09-13 11:33:19 -0600

    Add initial HTML and CSS for third page with cards

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-var playGameBtn = document.querySelector('#play-game-btn');
+var playGameBtn = document.querySelector('.play-game-btn');
+
+playGameBtn.addEventListener('click', showRulePage);
+// window.addEventListener('load', hideRulePage);
 
 
-playGameBtn.addEventListener('click', showName);
 
-function showName() {
+function showRulePage() {
+  // var rulesPage = document.querySelector('.rule-page');
+  console.log('test');
+  var welcomePage = document.querySelector('.first-page');
+  var rulePage = document.querySelector('.rule-page');
+
   var player1name = document.querySelector('.player1-name-input');
   var player1nameDisplay = document.querySelector('.player1-name');
   var errorMsg = document.querySelector('.hide-msg');
   if (player1name.value.length) {
+    welcomePage.classList.add('hide');
+    rulePage.classList.remove('hide');
     player1nameDisplay.innerHTML = player1name.value.toUpperCase();
-    errorMsg.style.display = 'none';
   } else {
     errorMsg.style.display = 'block';
   }
