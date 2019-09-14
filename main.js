97edf7d004a51d737e4bcbf8db9c368bae80abc7
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-09-13 21:36:56 -0600

    Fix aside CSS, update pictures, show pictures on event.target eventListener

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-var playGameBtn = document.querySelector('.play-game-btn');
+var playGameBtn1 = document.querySelector('.play-game-btn');
+var playGameBtn2 = document.querySelector('#play-game-btn2');
+var rulePage = document.querySelector('.rule-page');
+var cardContainer = document.querySelector('#card-container');
+var picturesArr = ['images/ears.JPG', 'images/ears.JPG', 'images/clouds.JPG', 'images/clouds.JPG', 'images/snow.JPG', 'images/snow.JPG', 'images/hat.JPG', 'images/hat.JPG', 'images/stick.JPG','images/stick.JPG'];
 
-playGameBtn.addEventListener('click', showRulePage);
-// window.addEventListener('load', hideRulePage);
+playGameBtn1.addEventListener('click', showRulePage);
+playGameBtn2.addEventListener('click', showCardPage);
+cardContainer.addEventListener('click', showPicture);
 
 
 
 function showRulePage() {
-  // var rulesPage = document.querySelector('.rule-page');
-  console.log('test');
   var welcomePage = document.querySelector('.first-page');
-  var rulePage = document.querySelector('.rule-page');
-
   var player1name = document.querySelector('.player1-name-input');
   var player1nameDisplay = document.querySelector('.player1-name');
   var errorMsg = document.querySelector('.hide-msg');
@@ -22,3 +23,24 @@ function showRulePage() {
     errorMsg.style.display = 'block';
   }
 };
+
+function showCardPage() {
+  var cardPage = document.querySelector('.card-page');
+  rulePage.classList.add('hide');
+  cardPage.classList.remove('hide');
+};
+
+function showPicture(event) {
+  var randomIndex = Math.floor(Math.random() * picturesArr.length);
+  var cardPic = picturesArr[randomIndex];
+  picturesArr.splice(randomIndex, 1);
+  event.target.src = cardPic;
+
+  if (event.target.src === cardPic) {
+    event.target.src = 'images/L.jpg';
+  }
+};
+
+function hidePicture() {
+
+}
